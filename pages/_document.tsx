3e85b95f4586032570d9fb1b0d444505902187fb
Rade58 <bajic.rade2@gmail.com>
2021-08-15 01:34:31 +0200

    🦉

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
