012341481a696a931265cf372a8c17f60010e214
Rade58 <bajic.rade2@gmail.com>
2021-08-15 15:59:11 +0200

    🦉

---
+++ theme/index.tsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@material-ui/core/styles";
+
+import gray from "@material-ui/core/colors/grey";
+
+const theme = createTheme({
+  // I WON'T BE ADD ANYTHING
+  // BUT YOU CAN EXTEND EVERYTHING
+  //
+});
+
+export default theme;
