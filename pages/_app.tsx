2c5057325391fde5be5fe1dce79776143f2d3f28
Rade58 <bajic.rade2@gmail.com>
2021-08-15 16:13:30 +0200

    🦉

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
-import "../styles/globals.css";
+// import "../styles/globals.css";
+import { useEffect, Fragment } from "react";
 import type { AppProps } from "next/app";
 
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+
+import theme from "../theme";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Fragment>
+  );
 }
 export default MyApp;
