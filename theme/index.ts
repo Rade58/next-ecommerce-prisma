10f5f09d3a0577c3d7afecee94bc5c59902c97c9
Rade58 <bajic.rade2@gmail.com>
2021-08-15 20:04:16 +0200

    🦉

--- theme/index.ts
+++ theme/index.ts
@@ -4,6 +4,14 @@ import gray from "@material-ui/core/colors/grey";
 
 import lime from "@material-ui/core/colors/lime";
 
+enum colors_enum {
+  dark_byzantium = "#542344",
+  dark_purple = "#260f26",
+  linen = "#efe6dd",
+  nickel_grey = "#626c66",
+  rifle_green = "#434a42",
+}
+
 const theme = createTheme({
   // I WON'T BE ADD ANYTHING
   // BUT YOU CAN EXTEND EVERYTHING
@@ -15,7 +23,9 @@ const theme = createTheme({
     secondary: {
       main: lime["200"],
     },
-    background: {},
+    background: {
+      // default:
+    },
   },
 });
 
