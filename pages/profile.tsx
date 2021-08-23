1f1f5207f2c0db9d2bf2b3df8a9e6d73e4b0fad9
Rade58 <bajic.rade2@gmail.com>
2021-08-24 01:38:22 +0200

    🦉

--- pages/profile.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint react/react-in-jsx-scope: 0 */
-/* eslint jsx-a11y/anchor-is-valid: 1 */
-import type { NextPage as NP } from "next";
-
-const ProfilePage: NP = () => {
-  return <div>profile: 👾</div>;
-};
-
-export default ProfilePage;
