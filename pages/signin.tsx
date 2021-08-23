e56a871c86d8f18850d7a416151bf86b1363f625
Rade58 <bajic.rade2@gmail.com>
2021-08-23 17:23:57 +0200

    🦉

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,9 +1,113 @@
 /* eslint react/react-in-jsx-scope: 0 */
 /* eslint jsx-a11y/anchor-is-valid: 1 */
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
 import type { NextPage as NP } from "next";
+import { useState, useCallback } from "react";
 
-const SigninPage: NP = () => {
-  return <div>SignIn👾</div>;
+import type { ChangeEventHandler, FormEvent } from "react";
+
+import { TextField, Button, CircularProgress } from "@material-ui/core";
+
+import axios from "axios";
+
+const SignInPage: NP = () => {
+  const [{ email }, setFields] = useState<{
+    email: string;
+  }>({
+    email: "",
+  });
+
+  const [reqStatus, setReqStatus] = useState<"idle" | "pending">("idle");
+
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) =>
+    setFields((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+
+  const handleSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      setReqStatus("pending");
+      try {
+        //
+      } catch (err) {
+        setReqStatus("idle");
+        //
+      }
+    },
+    [email, setReqStatus]
+  );
+
+  const buttonDisabled = !email || reqStatus === "pending" ? true : false;
+
+  return (
+    <main>
+      <h1>Sign In</h1>
+      <section
+        className="form-holder"
+        css={css`
+          padding-top: 10vh;
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          align-content: center;
+
+          & div.field {
+            margin-top: 10vh;
+            display: flex;
+            justify-content: center;
+          }
+
+          & button {
+            margin-top: 8vh;
+          }
+        `}
+      >
+        <form onSubmit={handleSubmit}>
+          <div className="field">
+            <TextField
+              onChange={handleChange}
+              value={email}
+              type="email"
+              name="email"
+              id="email-field"
+              label="Send To Email Address"
+              placeholder="Send To Email address"
+              variant="filled"
+            />
+          </div>
+
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            disabled={buttonDisabled}
+          >
+            {"Sign In "}
+            {reqStatus === "pending" ? (
+              <div
+                css={css`
+                  display: inline-block;
+                  margin-left: 8px;
+                `}
+              >
+                <CircularProgress color="primary" size={18} />
+              </div>
+            ) : (
+              ""
+            )}
+          </Button>
+        </form>
+      </section>
+    </main>
+  );
 };
 
-export default SigninPage;
+export default SignInPage;
