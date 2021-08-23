142ea11e2e5faac97f128b24613f7ba7e4beb8df
Rade58 <bajic.rade2@gmail.com>
2021-08-23 23:22:06 +0200

    🦉

---
+++ components/ProfileMenu.tsx
@@ -0,0 +1,45 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+import { useState } from "react";
+import type { FC, MouseEvent } from "react";
+
+import { Button, Menu, MenuItem } from "@material-ui/core";
+
+const ProfileMenu: FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Open Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default ProfileMenu;
---
+++ pages/profile.tsx
@@ -0,0 +1,9 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import type { NextPage as NP } from "next";
+
+const ProfilePage: NP = () => {
+  return <div>👾</div>;
+};
+
+export default ProfilePage;
