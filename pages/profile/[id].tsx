55a6326b4a73a85168861d435acb3eba83dabbd6
Rade58 <bajic.rade2@gmail.com>
2021-08-24 02:17:41 +0200

    🦉

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -4,29 +4,74 @@ import type { GetServerSideProps, NextPage as NP } from "next";
 
 import { useRouter } from "next/router";
 
+// CONVINIENTLY WE HAVE TYPE GENERATED FOR US
+import type { Profile, User } from "@prisma/client";
+
+// WE NEED SESSION
+import { getSession } from "next-auth/client";
+
+// WE NEED PRISMA CLIENT
+import prismaClient from "../../lib/prisma";
+
 interface PropsI {
-  placeholder: boolean;
+  profile: Profile & {
+    user: User;
+  };
 }
 
-/* type paramsType = {
-  siteId: string;
+type paramsType = {
+  // SAME NAME AS [id].tsx
+  id: string;
 };
 
-export const getServerSideProps: GetServerSideProps<PropsI, paramsType> =
+export const getServerSideProps: GetServerSideProps<PropsI | {}, paramsType> =
   async (ctx) => {
     const { params } = ctx;
 
-    params?.siteId; //
+    const profileId = params?.id || "";
+
+    // GETTING SESSION
+    // SESSION SHOUD HAVE userId
+    // BECAUSE WE INSERTED IT THER THROU session CALLLBACK
+    // IN NEXT-AUTH CONFIGURATION
+    const session = (await getSession()) || { userId: "" };
+
+    // LET'S FETCH PROFILE
+    // WE CAN DO A JOIN TO GET User RECORD TOGETHER
+    // WITH Profile RECORD
+    // BECAUSE Profile RECORD HAS A FORAIGN KEY OF User (id)
+
+    const profileWithUser = await prismaClient.profile.findUnique({
+      where: {
+        id: profileId,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    // LET'S DO REDIRECT IF THERE IS NO PROFILE
+    if (!profileWithUser) {
+      ctx.res.writeHead(302, { Location: "/" });
+
+      return {
+        props: {
+          profile: {},
+        },
+      };
+    }
 
     return {
       props: {
-        placeholder: true,
+        profile: profileWithUser,
       },
     };
-  }; */
+  };
 
 const ProfilePage: NP<PropsI> = (props) => {
   //
+
+  const { profile } = props;
 
   const { query } = useRouter();
 
@@ -36,7 +81,7 @@ const ProfilePage: NP<PropsI> = (props) => {
     <div>
       🦉
       <br />
-      {JSON.stringify({ query }, null, 2)}
+      {JSON.stringify({ profile }, null, 2)}
     </div>
   );
 };
