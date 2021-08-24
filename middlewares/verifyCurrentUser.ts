779c1676d552f1b5b69a39f70c67c7fe9ba1b433
Rade58 <bajic.rade2@gmail.com>
2021-08-24 23:50:26 +0200

    🦉

--- middlewares/verifyCurrentUser.ts
+++ middlewares/verifyCurrentUser.ts
@@ -1,17 +1,49 @@
-import nc from "next-connect";
 import type { Middleware } from "next-connect";
 import type { NextApiRequest, NextApiResponse } from "next";
+
+import { getSession } from "next-auth/client";
+
+import prismaClient from "../lib/prisma";
 
 const verifyCurrentUser: Middleware<NextApiRequest, NextApiResponse> = async (
   req,
   res,
   next
 ) => {
-  // VERIFY CURRENT USER
+  // TAKING ID FROM ROUTE
+  const { id } = req.query;
+  console.log({ id });
+  // TAKING SESSION
+  const session = (await getSession({ req })) as {
+    profile: { id: string };
+    userId: string;
+  };
+  console.log({ session });
+  // CHECKING profile
+  if (id !== session.profile.id) {
+    return res.status(401).send("UNAUTHORIZED");
+  }
 
-  console.log("HELLO USER");
+  // OBTAINING USER
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: session.userId,
+    },
+    select: {
+      id: true,
+    },
+  });
+  //
 
-  next("UNAUTHORIZED");
+  console.log({ user });
+
+  // IF THERE IS NO USER
+
+  if (!user || !user.id) {
+    return res.status(401).send("UNAUTHORIZED");
+  }
+
+  next();
 };
 
 export default verifyCurrentUser;
