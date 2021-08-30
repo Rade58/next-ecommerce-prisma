fc6bd767057e39174e3e89e39a6256ee1eca7e30
Rade58 <bajic.rade2@gmail.com>
2021-08-30 03:29:10 +0200

    🦉

--- pages/api/admin/[id].tsx
+++ pages/api/admin/[id].tsx
@@ -7,6 +7,18 @@ import prismaClient from "../../../lib/prisma";
 
 const handler = nc<NextApiRequest, NextApiResponse>();
 
+handler.delete(async (req, res) => {
+  const { id } = req.query;
+
+  if (!id) return res.status(403).send("unauthorized");
+
+  const body = req.body;
+
+  console.log(JSON.stringify({ id, body }, null, 2));
+
+  res.status(200).end();
+});
+
 // I ADDED MIDDLEWARE LIKE YOU SEE
 handler.use(verifyCurrentUser);
 
