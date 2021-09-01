48385264548be4edd9c64508ecbf34cf6e2d819d
Rade58 <bajic.rade2@gmail.com>
2021-09-02 00:28:06 +0200

    🦉

--- components/1_index_page/Products.tsx
+++ components/1_index_page/Products.tsx
@@ -17,6 +17,8 @@ import { Alert } from "@material-ui/lab";
 
 import axios from "axios";
 
+import { useSession } from "next-auth/client";
+
 // import type { ProductsListType } from "../../dummy/products";
 
 import type { Products as ProductsType } from "../../pages/index";
@@ -28,6 +30,8 @@ const LatestProducts: FC<{
   const [products, setProducts] = useState<typeof prods>(prods);
 
   // console.log({ products });
+
+  const [session, loading] = useSession();
 
   const [requestStatus, setRequestStatus] = useState<
     "idle" | "pending" | "rejected"
@@ -86,18 +90,27 @@ const LatestProducts: FC<{
   useEffect(() => {
     if (!window.onscroll) {
       window.onscroll = () => {
+        console.log(requestStatusRef.current);
+
         if (requestStatusRef.current === "pending") return;
 
         if (
           document.documentElement.scrollHeight -
             (document.documentElement.scrollTop +
-              document.documentElement.clientHeight) ===
-          0
+              document.documentElement.clientHeight) <
+          document.documentElement.clientHeight
         ) {
           // console.log("fetch data");
 
           fetchNewProducts();
         }
+        /* 
+        console.log(
+          document.documentElement.scrollHeight -
+            (document.documentElement.scrollTop +
+              document.documentElement.clientHeight) <
+            document.documentElement.clientHeight
+        ); */
       };
     }
 
@@ -113,7 +126,7 @@ const LatestProducts: FC<{
         /* width: fit-content; */
         /* text-align: center; */
 
-        border: pink solid 1px;
+        /* border: pink solid 1px; */
 
         width: 100%;
 
@@ -131,64 +144,66 @@ const LatestProducts: FC<{
       <Typography variant="h2" component="h6">
         Latest Products
       </Typography>
-      <div
-        css={css`
-          border: crimson solid 1px;
-          width: 75%;
+      {!loading && (
+        <div
+          css={css`
+            /* border: crimson solid 1px; */
+            width: 75%;
 
-          @media screen and (max-width: 600px) {
-            width: 94%;
-          }
+            @media screen and (max-width: 600px) {
+              width: 94%;
+            }
 
-          margin: 20px auto;
+            margin: 20px auto;
 
-          justify-content: space-evenly;
+            justify-content: space-evenly;
 
-          display: flex;
-          flex-wrap: wrap;
+            display: flex;
+            flex-wrap: wrap;
 
-          & > div.my-card {
-            flex-basis: 280px;
-            flex-grow: 1;
-            flex-shrink: 2;
-            /* border: pink solid 2px; */
+            & > div.my-card {
+              flex-basis: 280px;
+              flex-grow: 1;
+              flex-shrink: 2;
+              /* border: pink solid 2px; */
 
-            margin: 16px;
-          }
-        `}
-      >
-        {products.map((product) => {
-          return (
-            <ProductCard
-              product={product}
-              key={`${product.productId}-${Math.random()}-${product.name}`}
-            />
-          );
-        })}
-        {requestStatus === "pending" && (
-          <div
-            css={css`
-              z-index: 4;
-              display: flex;
-              justify-content: center;
-              position: fixed;
+              margin: 16px;
+            }
+          `}
+        >
+          {products.map((product) => {
+            return (
+              <ProductCard
+                product={product}
+                key={`${product.productId}-${Math.random()}-${product.name}`}
+              />
+            );
+          })}
+          {requestStatus === "pending" && (
+            <div
+              css={css`
+                z-index: 4;
+                display: flex;
+                justify-content: center;
+                position: fixed;
 
-              bottom: 12vh;
+                bottom: 12vh;
 
-              & > * {
-                text-align: center;
-              }
-            `}
-          >
-            <CircularProgress color="primary" size={48} />
-          </div>
-        )}
-        {requestStatus === "rejected" && (
-          <Alert severity="error">
-            Could{"'"}t fetch products (server problem)
-          </Alert>
-        )}
-      </div>
+                & > * {
+                  text-align: center;
+                }
+              `}
+            >
+              <CircularProgress color="primary" size={48} />
+            </div>
+          )}
+          {requestStatus === "rejected" && (
+            <Alert severity="error">
+              Could{"'"}t fetch products (server problem)
+            </Alert>
+          )}
+        </div>
+      )}
     </div>
   );
 };
