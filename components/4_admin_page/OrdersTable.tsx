8ed9f9ca22f11b454e7125ba2940e60dee62a93e
Rade58 <bajic.rade2@gmail.com>
2021-09-04 01:37:32 +0200

    🦉

--- components/4_admin_page/OrdersTable.tsx
+++ components/4_admin_page/OrdersTable.tsx
@@ -76,34 +76,6 @@ const columns: GridColDef[] = [
   },
 ];
 
-/* const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-]; */
-
-/* export type ProfilesI = {
-  userId: string;
-  id: number;
-  email: string | null;
-  name: string | null;
-  profileId: string;
-  addrss: string | null;
-  city: string | null;
-  country: string | null;
-  paymentMethod: string | null;
-  postalCode: string | null;
-  role: Role;
-  taxPrice: number | null;
-  user: "";
-}[]; */
-
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
