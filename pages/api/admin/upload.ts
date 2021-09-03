10d67c956cbb2dbc856c4fac1f01c1baa8cd18e0
Rade58 <bajic.rade2@gmail.com>
2021-09-03 18:22:30 +0200

    🦉

--- pages/api/admin/upload.ts
+++ pages/api/admin/upload.ts
@@ -4,16 +4,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 // WE WILL USE MULTER LIKE I SAID
 import multer from "multer";
-import express from "express";
-// import type { Multer } from "multer";
-// import type { Express } from "express";
+//
 
-//
-// import verifyCurrentUser from "../../../middlewares/verifyCurrentUser";
-//
-// import prismaClient from "../../../lib/prisma";
-//
-// import type { UpdateProductsDataRecord } from "../../../components/4_admin_page/ProductsTable";
+import verifyCurrentUser from "../../../middlewares/verifyCurrentUser";
 
 const handler = nc<NextApiRequest, NextApiResponse>({
   onError(error, req, res) {
@@ -24,7 +17,6 @@ const handler = nc<NextApiRequest, NextApiResponse>({
       .json({ error: `Sorry something Happened! ${error.message}` });
   },
   onNoMatch(req, res) {
-    // console.log(error);
     res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
   },
 });
@@ -36,13 +28,18 @@ const storage = multer.diskStorage({
     cb(
       null,
       // SECOND ARGUMENT IS OUR FOLDER WE CREATED NOT LONG AGO
-      "./public/uploads/"
+      // BUT DON'T FORGET THAT WE SERVE STATIC FILES FROM public
+      // FOLDER
+      // IN OUR CASE WE WILL UPLOAD TO /public/uploads
+      "./public/uploads/" // (DONT FORGET . (DOT))
+      // BUT LATTER ON WHEN WE USE IMAGE AT FRONTEND
+      // WE LOAD IMAGE WITHOUT public PART OF THE PATH
     );
   },
   // FILENAME CALLBACK
   filename(req, file, cb) {
     // WE NEED TO FORMAT FILE NAME
-    // BECAUSE WHAT IF SOMEONE NAMES FILE WITH A NAME
+    // BECAUSE WHAT IF SOMEONE NAMES THEIR FILE WITH A NAME
     // THAT ALLREADY EXISTS FOR SOME OTHER FILE
 
     // AND WE WILL USE path.extname MODULE, TO GET EXTENSION FROM THE ORIGINAL NAME
@@ -58,19 +55,19 @@ const storage = multer.diskStorage({
 // LETS BUILD A MIDDLEWARE
 const upload = multer({
   storage,
-  // BUT E NEED TO RESTRICT SOME FILES FROM BEING UPLOADED
-  // WE WANT JUST IMAGES
+  // BUT WE NEED TO RESTRICT SOME FILES FROM BEING UPLOADED
+  // WE WANT JUST IMAGES, NOT OTHER TYPES OF FILES
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   },
 });
 
-// BUILDING THAT FILTERING FUNCTION WE CALLED IN fileFlter CALLBACJ
+// FILTERING FUNCTION WE ALREADY CALLED ABOVE INSIDE fileFlter CALLBAC
 function checkFileType(
   file: Express.Multer.File,
   cb: multer.FileFilterCallback
 ) {
-  const filetypes = /jpeg|jpg|png/;
+  const filetypes = /jpeg|jpg|png|svg/;
 
   const correctExtname = filetypes.test(
     path.extname(file.originalname).toLowerCase()
@@ -87,32 +84,14 @@ function checkFileType(
 
 // -------------
 
-// SIMULATING __dirname (BECAUSE IT IS NOT AVAILABLE WHEN WE USE import/export (I ASUME))
+handler.use(verifyCurrentUser);
 
-// MARKING STATIC FOLDER
-// handler.use(
-// "/public/uploads",
-// express.static(
-// path.join(
-// /* this argument is __dirname */ /*__dirname*/ path.resolve(),
-// "/public/uploads"
-// )
-// )
-// );
-
-// handler.use(verifyCurrentUser);
-
-// ADDING MIDDLEWARE (FOR UPLOADING OF SINGLE FILE)
-// I ADDED NAME IMAGE AND THT MEANS WHEN WE UPLOAD FROM THE FRONT END
-// WE NEED TO NAME IT image
+// ADDING MIDDLEWARE (FOR UPLOADING ONLY OF SINGLE FILE)
+// I ADDED FIELD NAME: "image", BECAUSE THAT MEANS WHEN WE UPLOAD FROM THE FRONT END
+// WE NEED TO NAME FIELD AS  `image`
 handler.use(upload.single("image"));
 
 handler.post(async (req, res) => {
-  // SO WE SHOULD HAVE ACCES OF THE IMAGE PATH IN HERE
-  // AN IT SHOULD BE ON REQUEST OBJECT LIKE THIS
-
-  console.log("POST IS WORKING");
-
   interface File {
     file: Express.Multer.File;
   }
@@ -120,11 +99,12 @@ handler.post(async (req, res) => {
   interface Req extends NextApiRequest, File {
     //
   }
-
-  // const imagePath = (req as unknown as {file: Express.Multer.File}).file.path
+  // SO WE SHOULD HAVE ACCES OF THE IMAGE PATH IN HERE
+  // AN IT SHOULD BE ON REQUEST OBJECT LIKE THIS
 
   let imagePath = (req as Req).file.path;
 
+  // WE WANT TO REMOVE /paublic FROM THE PATH
   const toCutString = "public";
   const val = toCutString.length;
 
@@ -133,15 +113,16 @@ handler.post(async (req, res) => {
   // WE WILL SEND BACK IMAGE PATH
   // BECAUSE WE ARE GOING TO STORE THAT PATH INSIDE Product RECORD
 
-  console.log({ imagePath });
+  // console.log({ imagePath });
 
   return res.status(201).send(imagePath);
-
-  //
 });
 
 export default handler;
 
+// WE WILL RECONFIGURE OUT ROUTE
+// BECAUSE SINCE WE USE MULTIPART FORM DATA, WE DON'T WANT
+// BODY PARSER
 export const config = {
   api: {
     bodyParser: false, // Disallow body parsing, consume as stream
