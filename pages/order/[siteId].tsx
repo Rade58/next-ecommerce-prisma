86277f2006708f5a0b53d09af6fe1427bf4bcca7
Rade58 <bajic.rade2@gmail.com>
2021-09-04 03:39:45 +0200

    🦉

---
+++ pages/order/[siteId].tsx
@@ -0,0 +1,38 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import type { GetServerSideProps, NextPage as NP } from "next";
+
+import { useRouter } from "next/router";
+
+interface PropsI {
+  placeholder: boolean;
+}
+
+type paramsType = {
+  siteId: string;
+};
+
+export const getServerSideProps: GetServerSideProps<PropsI, paramsType> =
+  async (ctx) => {
+    const { params } = ctx;
+
+    params?.siteId; //
+
+    return {
+      props: {
+        placeholder: true,
+      },
+    };
+  };
+
+const Page: NP<PropsI> = (props) => {
+  //
+
+  const { query } = useRouter();
+
+  console.log(props);
+
+  return <div>Order ID: {query.siteId}</div>;
+};
+
+export default Page;
