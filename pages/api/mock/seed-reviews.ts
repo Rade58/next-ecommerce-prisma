a482191fcacd725a470ab474c09b138507502e7a
Rade58 <bajic.rade2@gmail.com>
2021-09-04 22:59:27 +0200

    🦉

--- pages/api/mock/seed-reviews.ts
+++ pages/api/mock/seed-reviews.ts
@@ -8,23 +8,32 @@ import dummyReviews from "../../../dummy/mock/MOCK_DATA_REVIEWS.json";
 const handler = nc<NextApiRequest, NextApiResponse>();
 
 handler.get(async (req, res) => {
-  // LETS QUERY FOR ONLY ONE USER
+  // LETS QUERY FOR TWO USERS
   // HE IS GOING TO LEAVE ALL THE REVIEWS
 
-  const profile = (
+  const profile1 = (
     await prismaClient.profile.findMany({
-      where: {
-        user: {
-          email: {
-            equals: "bajic.rade2@gmail.com",
-          },
-        },
-      },
       select: {
         id: true,
       },
+      orderBy: {
+        updatedAt: "asc",
+      },
+      take: 10,
     })
-  )[0];
+  )[8];
+
+  const profile2 = (
+    await prismaClient.profile.findMany({
+      select: {
+        id: true,
+      },
+      orderBy: {
+        updatedAt: "asc",
+      },
+      take: 14,
+    })
+  )[12];
 
   // FIRST LETS QUERY FOR SOME PRODUCTS
   // FIRST 5 PRODUCTS WILL DO THE TRICK
@@ -46,9 +55,9 @@ handler.get(async (req, res) => {
   let orderIds: string[] = [];
 
   for (let i = 0; i < 20; i++) {
-    const order = await prismaClient.order.create({
+    const order1 = await prismaClient.order.create({
       data: {
-        buyerId: profile.id,
+        buyerId: profile1.id,
         isDelivered: true,
         // LETS MARK ORDER AS DELIVERED
         deliveredAt: new Date(Date.now()),
@@ -58,7 +67,19 @@ handler.get(async (req, res) => {
       },
     });
 
-    orderIds.push(order.id);
+    const order2 = await prismaClient.order.create({
+      data: {
+        buyerId: profile1.id,
+        isDelivered: true,
+        // LETS MARK ORDER AS DELIVERED
+        deliveredAt: new Date(Date.now()),
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    orderIds.push(order1.id, order2.id);
   }
 
   // LETS CREATE ORDER ELEMENTS
@@ -81,7 +102,20 @@ handler.get(async (req, res) => {
   for (let i = 0; i < dummyReviews.length; i++) {
     const review = await prismaClient.review.create({
       data: {
-        userId: profile.id,
+        userId: profile1.id,
+        productId: products[i].productId,
+        comment: dummyReviews[i].comment,
+        rating: dummyReviews[i].rating,
+      },
+    });
+
+    console.log(review);
+  }
+
+  for (let i = dummyReviews.length - 1; i > 0; i--) {
+    const review = await prismaClient.review.create({
+      data: {
+        userId: profile2.id,
         productId: products[i].productId,
         comment: dummyReviews[i].comment,
         rating: dummyReviews[i].rating,
