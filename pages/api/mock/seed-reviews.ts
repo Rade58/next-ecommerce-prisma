d92fd47dd09a50c5dd159969017ad872e4738901
Rade58 <bajic.rade2@gmail.com>
2021-09-04 16:37:28 +0200

    🦉

--- pages/api/mock/seed-reviews.ts
+++ pages/api/mock/seed-reviews.ts
@@ -8,8 +8,17 @@ import dummyReviews from "../../../dummy/mock/MOCK_DATA_REVIEWS.json";
 const handler = nc<NextApiRequest, NextApiResponse>();
 
 handler.get(async (req, res) => {
+  // LETS QUERY FOR ONLY ONE USER
+  // HE IS GOING TO LEAVE ALL THE REVIEWS
+
   // FIRST LETS QUERY FOR SOME PRODUCTS
   // FIRST 20 PRODUCTS WILL DO THE TRICK
+
+  // LETS CREATE ORDERS FOR THAT PRODUCTS
+
+  // LETS MARK ORDERS AS DELIVERED
+
+  // AND NOW WE CAN CREATE REVIEWS
 
   /* const products = await prismaClient.product.findMany({
 
