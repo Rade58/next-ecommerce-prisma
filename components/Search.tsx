0057dacfebeb247fe56e6b82c01a23b3c91bdb4d
Rade58 <bajic.rade2@gmail.com>
2021-09-05 18:43:29 +0200

    🦉

--- components/Search.tsx
+++ components/Search.tsx
@@ -4,12 +4,60 @@
 import { jsx, css } from "@emotion/react";
 import type { FC } from "react";
 
-import { TextField } from "@material-ui/core";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+
+import {
+  TextField,
+  Paper,
+  InputBase,
+  Divider,
+  IconButton,
+} from "@material-ui/core";
+
+import { Menu as MenuIcon, Search as SearchIcon } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: "2px 4px",
+      display: "flex",
+      alignItems: "center",
+      width: 400,
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+  })
+);
 
 const Search: FC = () => {
+  const classes = useStyles();
   return (
     <section className="search-box">
-      <TextField />
+      {/* <TextField /> */}
+      <Paper component="form" className={classes.root}>
+        <InputBase
+          className={classes.input}
+          placeholder="Search Products"
+          inputProps={{ "aria-label": "search google maps" }}
+        />
+        <IconButton
+          type="submit"
+          className={classes.iconButton}
+          aria-label="search"
+        >
+          <SearchIcon />
+        </IconButton>
+        <Divider className={classes.divider} orientation="vertical" />
+      </Paper>
     </section>
   );
 };
