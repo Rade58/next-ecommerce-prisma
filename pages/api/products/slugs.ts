16503d53ff1623c566939978140b834f12f3dba1
Rade58 <bajic.rade2@gmail.com>
2021-09-06 01:54:02 +0200

    🦉

--- pages/api/products/slugs.ts
+++ pages/api/products/slugs.ts
@@ -2,6 +2,7 @@ import nc from "next-connect";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import prismaClient from "../../../lib/prisma";
+import { supabase as supaClient } from "../../../lib/supabase";
 
 const handler = nc<NextApiRequest, NextApiResponse>();
 
@@ -9,15 +10,24 @@ handler.post(async (req, res) => {
   const { text } = req.body as { text: string };
 
   try {
-    const slugs = await prismaClient.$queryRaw<
-      { value: string; label: string }[]
-    >(/* sql */ `
-      SELECT "public"."Product"."productId" AS value,
-            name AS label FROM "public"."Product"
-      WHERE to_tsvector(name) @@ to_tsquery(${text});
-    `);
+    //  const slugs = await prismaClient.$queryRaw<
+    //    { value: string; label: string }[]
+    //  >(/* sql */ `
+    //    SELECT "public"."Product"."productId" AS value,
+    //          name AS label FROM "public"."Product"
+    //    WHERE to_tsvector(name) @@ to_tsquery(${text});
+    //  `);
 
-    return res.status(200).json(slugs);
+    // const slugs = await supaClient
+    // .from('"public"."Product"')
+    // .select()
+    // .textSearch("name", `'${text}'`);
+
+    const slugs = await supaClient.from("users").select("name").limit(10);
+
+    console.log(slugs);
+
+    return res.status(200).json("some");
   } catch (err) {
     console.error(err);
 
