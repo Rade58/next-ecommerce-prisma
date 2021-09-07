ff2a67c5a0e5691c250fbba28c89ddad9e3dce92
Rade58 <bajic.rade2@gmail.com>
2021-09-07 19:33:06 +0200

    🦉

--- components/1_index_page/Carousel.tsx
+++ components/1_index_page/Carousel.tsx
@@ -5,8 +5,57 @@ import { jsx, css } from "@emotion/react";
 import type { FC } from "react";
 import { useState, useCallback, useEffect } from "react";
 
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+import Car from "react-material-ui-carousel";
+
+import { Paper, Button } from "@material-ui/core";
+
+const CarItem: FC<{
+  item: {
+    name: string;
+    description: string;
+    image: string;
+  };
+}> = (props) => {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  );
+};
+
 const Carousel: FC = () => {
-  return null;
+  var items = [
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+      image: "",
+      src: "",
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!",
+      image: "",
+      src: "",
+    },
+  ];
+
+  return (
+    <Carousel>
+      {items.map((item, i) => (
+        <Link href="/prods/1" key={`${i}-${item.name}`}>
+          <a>
+            <CarItem key={i} item={item} />
+          </a>
+        </Link>
+      ))}
+    </Carousel>
+  );
 };
 
 export default Carousel;
