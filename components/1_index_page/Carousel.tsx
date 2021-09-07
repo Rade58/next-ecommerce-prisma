10050e473a6cd7026980a0f7af171bc56feaad80
Rade58 <bajic.rade2@gmail.com>
2021-09-08 00:46:45 +0200

    🦉

--- components/1_index_page/Carousel.tsx
+++ components/1_index_page/Carousel.tsx
@@ -5,7 +5,7 @@ import { jsx, css } from "@emotion/react";
 import type { FC } from "react";
 import { useState, useCallback, useEffect, Fragment } from "react";
 
-import { useRouter } from "next/router";
+import Router from "next/router";
 import Link from "next/link";
 
 import Image from "next/image";
@@ -16,35 +16,41 @@ import { Paper, Button } from "@material-ui/core";
 
 const placeholderImage = "https://placeimg.com/640/480/any";
 
-var items = [
+interface ProductItemI {
+  name: string;
+  description: string;
+  image: string;
+  prodId: string;
+}
+
+var placeholderItems: ProductItemI[] = [
   {
     name: "Random Name #1",
     description: "Probably the most random thing you have ever seen!",
     image: placeholderImage,
     src: placeholderImage,
+    prodId: "",
   },
   {
     name: "Random Name #2",
     description: "Hello World!",
     image: placeholderImage,
     src: placeholderImage,
+    prodId: "",
   },
 ];
 
 const CarItem: FC<{
-  item: {
-    name: string;
-    description: string;
-    image: string;
-  };
+  item: ProductItemI;
 }> = (props) => {
   return (
     <section
       css={css`
         /* border: crimson solid 1px; */
-        margin-top: -18px;
+        /* margin-top: 12px; */
         /* height: 62vh; */
 
+        position: relative;
         display: flex;
 
         & .prod-desc {
@@ -53,9 +59,76 @@ const CarItem: FC<{
             rgba(245, 177, 97, 1) 0.4%,
             rgba(236, 54, 110, 1) 100.2%
           );
-          display: inline-block;
+          display: flex;
+          justify-content: space-between;
+          flex-direction: column;
           width: 36%;
           height: 60vh;
+
+          & h2 {
+            margin-bottom: 20px;
+            margin-top: 20px;
+            position: absolute;
+            top: 10%;
+            left: 20%;
+            max-width: 70%;
+            z-index: 200;
+            color: #95dfcfef;
+            font-size: 2.2em;
+            font-family: ubuntu, -apple-system, BlinkMacSystemFont, "Segoe UI",
+              Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+              sans-serif;
+            background-color: #6d79a01a;
+            /* padding: 10px; */
+            text-align: center;
+            /* border-radius: 20px; */
+            box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset,
+              rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+              rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset,
+              rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px,
+              rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px,
+              rgba(0, 0, 0, 0.09) 0px 32px 16px;
+            text-shadow: 1px 1px 1px black;
+          }
+          & p {
+            margin-bottom: 20px;
+            margin-top: 20px;
+            position: absolute;
+            top: 38%;
+            left: 60%;
+            max-width: 40%;
+            z-index: 200;
+            color: #95dfcfef;
+            font-family: ubuntu, -apple-system, BlinkMacSystemFont, "Segoe UI",
+              Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+              sans-serif;
+            font-size: 1.2em;
+
+            margin-right: 20px;
+            background-color: #6d79a021;
+            text-align: center;
+            border-radius: 2px;
+            /* padding: 10px; */
+            box-shadow: rgba(240, 46, 170, 0.4) -5px 5px,
+              rgba(240, 46, 170, 0.3) -10px 10px,
+              rgba(240, 46, 170, 0.2) -15px 15px,
+              rgba(240, 46, 170, 0.1) -20px 20px,
+              rgba(240, 46, 170, 0.05) -25px 25px;
+            text-shadow: 1px 1px 1px black;
+          }
+
+          & div.butt-holder {
+            width: 100%;
+            /* border: pink solid 4px; */
+            display: flex;
+            margin-top: auto;
+            margin-bottom: 10px;
+
+            & button {
+              margin-left: auto;
+              margin-right: 10%;
+            }
+          }
         }
 
         & .prod-image {
@@ -78,9 +151,17 @@ const CarItem: FC<{
       <div className="prod-desc">
         <h2>{props.item.name}</h2>
         <p>{props.item.description}</p>
-        <Button variant="outlined" color="secondary">
-          Check it out!
-        </Button>
+        <div className="butt-holder">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => {
+              Router.push(`/products/${props.item.prodId}`);
+            }}
+          >
+            Check it out!
+          </Button>
+        </div>
       </div>
       <div className="prod-image">
         <div>
@@ -103,17 +184,39 @@ const CarItem: FC<{
 const Carousel: FC = () => {
   const [mounted, setMounted] = useState<boolean>(false);
 
+  const [reqStatus, setReqStatus] = useState<"idle" | "pending" | "failed">(
+    "idle"
+  );
+
+  const [products, setProducts] = useState<ProductItemI[]>([]);
+
   useEffect(() => {
     if (!mounted) {
       setMounted(true);
     }
   }, [setMounted, mounted]);
 
+  useCallback(async () => {
+    try {
+    } catch (error) {
+      console.error(error);
+
+      setReqStatus("failed");
+    }
+  }, [setReqStatus]);
+
   return (
     <Fragment>
-      {mounted && (
+      {mounted && reqStatus === "idle" && (
         <Car>
-          {items.map((item, i) => (
+          {products.map((item, i) => (
+            <CarItem key={i} item={item} />
+          ))}
+        </Car>
+      )}
+      {reqStatus === "failed" && (
+        <Car>
+          {placeholderItems.map((item, i) => (
             <CarItem key={i} item={item} />
           ))}
         </Car>
