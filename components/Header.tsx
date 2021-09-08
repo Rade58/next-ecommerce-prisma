e06d7dc0b85ab09612a71ae3e011a9707a5ec026
Rade58 <bajic.rade2@gmail.com>
2021-09-08 18:17:53 +0200

    🦉

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,8 +42,6 @@ import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
 
 const useStyles = makeStyles({
   list: {
@@ -85,25 +83,17 @@ function TemporaryDrawer() {
       onKeyDown={toggleDrawer("right", false)}
     >
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem button>
+          <ListItemIcon>Content</ListItemIcon>
+          <ListItemText primary={"something"} />
+        </ListItem>
       </List>
       <Divider />
       <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem button>
+          <ListItemIcon>Content</ListItemIcon>
+          <ListItemText primary={"something"} />
+        </ListItem>
       </List>
     </div>
   );
