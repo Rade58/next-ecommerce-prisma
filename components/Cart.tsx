2528ec8fbe05c8ee5ab41abec3c36a80ad42e9b8
Rade58 <bajic.rade2@gmail.com>
2021-09-08 15:53:55 +0200

    🦉

---
+++ components/Cart.tsx
@@ -0,0 +1,11 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+import type { FC } from "react";
+
+const ShoppingCart: FC = () => {
+  return null;
+};
+
+export default ShoppingCart;
