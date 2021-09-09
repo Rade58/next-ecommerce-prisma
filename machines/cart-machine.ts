a43270dd4a38f3a3a63d3bac5c8c5cae65517887
Rade58 <bajic.rade2@gmail.com>
2021-09-09 17:53:42 +0200

    🦉

--- machines/cart-machine.ts
+++ machines/cart-machine.ts
@@ -1,5 +1,7 @@
 import { createMachine, assign, interpret } from "xstate";
 
+import axios from "axios";
+
 import CartStore from "../lib/cart-cookies";
 import type { CartRecord, ItemIn } from "../lib/cart-cookies";
 
@@ -16,6 +18,7 @@ export enum fse {
   moving_to_checkout = "moving_to_checkout",
   erasing_everything = "erasing_everything",
   stock_exceed = "stock_exceed",
+  request_failed = "request_failed",
 }
 
 /**
@@ -26,13 +29,14 @@ export enum EE {
   ADD_ITEM = "ADD_ITEM",
   REMOVE_ITEM = "REMOVE_ITEM",
   ERASE_EVERYTHING = "ERASE_EVERYTHING",
+  // SERVER_RESPONDED = "SERVER_RESSPONDED",
 }
 
 // TO BE USED AS GENERIC TYPES INSIDE STATE MACHINE DEFINISTION
 
 export interface MachineContextGenericI {
   cart: CartRecord;
-  lastItemId: string | undefined;
+  lastItemId: string;
 }
 
 export type machineEventsGenericType =
@@ -54,6 +58,12 @@ export type machineEventsGenericType =
         productId: string;
       };
     };
+/* | {
+      type: EE.SERVER_RESPONDED;
+      payload: {
+        productId: string;
+      };
+    } */
 
 export type machineFiniteStatesGenericType =
   | {
@@ -91,16 +101,17 @@ const cartMachine = createMachine<
   MachineContextGenericI,
   machineEventsGenericType,
   machineFiniteStatesGenericType
->({
-  id: "main_machine",
-  initial: fse.mounting_the_cart,
-  context: {
-    cart: CartStore.getCart(),
-    lastItemId: undefined,
-  },
-  // ---- EVENTS RECEVIED WHEN CURRENT FINITE STATE DOESN'T MATTER -----
-  on: {
-    /* [EE.CHECK_CURRENT_DARK_MODE]: {
+>(
+  {
+    id: "main_machine",
+    initial: fse.mounting_the_cart,
+    context: {
+      cart: CartStore.getCart(),
+      lastItemId: "",
+    },
+    // ---- EVENTS RECEVIED WHEN CURRENT FINITE STATE DOESN'T MATTER -----
+    on: {
+      /* [EE.CHECK_CURRENT_DARK_MODE]: {
       actions: [
         assign((ctx, event) => {
           const { isDark } = event.payload;
@@ -111,98 +122,180 @@ const cartMachine = createMachine<
         }),
       ],
     }, */
-  },
-  // -------------------------------------------------------------------
-  states: {
-    [fse.mounting_the_cart]: {
-      entry: [
-        assign({
-          cart: (_, __) => {
-            const currentCart = CartStore.getCart();
+    },
+    // -------------------------------------------------------------------
+    states: {
+      [fse.mounting_the_cart]: {
+        entry: [
+          assign({
+            cart: (_, __) => {
+              const currentCart = CartStore.getCart();
 
-            return currentCart;
+              return currentCart;
+            },
+          }),
+        ],
+        always: {
+          target: fse.idle,
+        },
+      },
+      [fse.idle]: {
+        //
+        on: {
+          [EE.ADD_ITEM]: {
+            actions: [
+              assign({
+                cart: (_, ev) => {
+                  const {
+                    item: { amount, productId },
+                  } = ev.payload;
+
+                  return CartStore.setCartItem({
+                    amount,
+                    productId,
+                  });
+                },
+                lastItemId: (_, ev) => {
+                  return ev.payload.item.productId;
+                },
+              }),
+            ],
+            target: fse.adding_item,
           },
-        }),
-      ],
-      always: {
-        target: fse.idle,
-      },
-    },
-    [fse.idle]: {
-      //
-      on: {
-        [EE.ADD_ITEM]: {
-          actions: [
-            assign({
-              cart: (_, ev) => {
-                const {
-                  item: { amount, productId },
-                } = ev.payload;
+          [EE.REMOVE_ITEM]: {
+            actions: [
+              assign({
+                cart: (_, ev) => {
+                  const { productId } = ev.payload;
 
-                return CartStore.setCartItem({
-                  amount,
-                  productId,
-                });
-              },
-              lastItemId: (_, ev) => {
-                return ev.payload.item.productId;
-              },
-            }),
-          ],
-          target: fse.adding_item,
-        },
-        [EE.REMOVE_ITEM]: {
-          actions: [
-            assign({
-              cart: (_, ev) => {
-                const { productId } = ev.payload;
-
-                return CartStore.removeCartItem(productId);
-              },
-              lastItemId: (_, ev) => {
-                return ev.payload.productId;
-              },
-            }),
-          ],
-          target: fse.removing_item,
-        },
-        [EE.ERASE_EVERYTHING]: {
-          actions: [
-            assign({
-              cart: (_, __) => {
-                return CartStore.clearCart();
-              },
-            }),
-          ],
-          target: fse.erasing_everything,
+                  return CartStore.removeCartItem(productId);
+                },
+                lastItemId: (_, ev) => {
+                  return ev.payload.productId;
+                },
+              }),
+            ],
+            target: fse.removing_item,
+          },
+          [EE.ERASE_EVERYTHING]: {
+            actions: [
+              assign({
+                cart: (_, __) => {
+                  return CartStore.clearCart();
+                },
+                lastItemId: (_, ev) => {
+                  return "";
+                },
+              }),
+            ],
+            target: fse.erasing_everything,
+          },
         },
       },
-    },
-    [fse.adding_item]: {
-      //
+      [fse.adding_item]: {
+        //
+        invoke: {
+          id: "add_item",
+          src: (ctx, event) => {
+            const { lastItemId, cart } = ctx;
 
-      /* invoke: {
+            const data = cart[lastItemId];
 
-      }, */
+            return axios.put(`/api/cart/${lastItemId}`, {
+              amount: data.amount,
+              type: "cart-add",
+            });
+            // JUST TO SAY (DATA SHOUD BE INSIDE
+            // AND YOU CAN DO ASSIGNS TO IN THESE ACTIONS
+            //   onDone: {actions: [  (ctx, event) => {  event.data  }  ] }             )
+            // MORE ON THAT HERE:
+            // https://xstate.js.org/docs/guides/communication.html#invoking-promises
+          },
 
-      always: {
-        target: fse.idle,
+          onDone: {
+            target: fse.idle,
+            // LIKE I SAID
+            // DATA SHOULD BE ACCESSIBLE HERE
+            // AND SINCE WE USEaxios DATA SHOUD BE IN   {data}
+            actions: [
+              (ctx, event) => {
+                // LIKE I SAID DATA IS HERE
+                console.log(
+                  //
+                  event.data.data
+                  //
+                );
+              },
+            ],
+          },
+          onError: {
+            target: fse.request_failed,
+            actions: [
+              (ctx, event) => {
+                // ERROR IS HERE (ALSO ON .data)
+                console.log(
+                  //
+                  event.data
+                  //
+                );
+              },
+            ],
+          },
+        },
       },
-    },
-    [fse.removing_item]: {
-      //
-      always: {
-        target: fse.idle,
+      [fse.removing_item]: {
+        //
+        invoke: {
+          id: "remove_item",
+          src: (ctx, event) => {
+            const { cart, lastItemId } = ctx;
+
+            const data = cart[lastItemId];
+
+            return axios.put(`/api/cart/${lastItemId}`, {
+              amount: data.amount,
+              type: "cart-remove",
+            });
+          },
+          onDone: {
+            target: fse.idle,
+            actions: [
+              (ctx, event) => {
+                console.log(event.data.data);
+              },
+            ],
+          },
+          onError: {
+            target: fse.request_failed,
+            actions: [
+              (ctx, event) => {
+                console.log(event.data);
+              },
+            ],
+          },
+        },
       },
-    },
-    [fse.erasing_everything]: {
-      //
-      always: {
-        target: fse.idle,
+      [fse.erasing_everything]: {
+        //
+        always: {
+          target: fse.idle,
+        },
+      },
+      [fse.request_failed]: {
+        after: {
+          TWOSECONDSTOIDLE: {
+            target: fse.idle,
+          },
+        },
       },
     },
   },
-});
+  {
+    delays: {
+      TWOSECONDSTOIDLE: 2000,
+    },
+  }
+);
 
 export const cartService = interpret(cartMachine);
 
