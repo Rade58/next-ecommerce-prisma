156278b9464eeafca40b17a22c72387be65db964
Rade58 <bajic.rade2@gmail.com>
2021-09-09 21:38:23 +0200

    🦉

--- lib/date/index.ts
+++ lib/date/index.ts
@@ -1,10 +1,18 @@
 export const afterDate = (milisecs?: number) => {
   //
-  let ms = milisecs ? milisecs : /* 24h -> */ 1000 * 60 * 60 * 24;
+  const ms = milisecs ? milisecs : /* 24h -> */ 1000 * 60 * 60 * 24;
 
   const currTime = new Date();
 
   const after = currTime.getTime() + ms;
 
   return new Date(after);
+};
+
+export const elapsed = (date: Date) => {
+  const currTime = new Date().getTime();
+
+  const time = date.getTime();
+
+  return time < currTime;
 };
