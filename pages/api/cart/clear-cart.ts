5afeef6592e01c2618cfe0f75c3f98c59604ef9c
Rade58 <bajic.rade2@gmail.com>
2021-09-12 15:17:19 +0200

    🦉

---
+++ pages/api/cart/clear-cart.ts
@@ -0,0 +1,57 @@
+import nc from "next-connect";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import prismaClient from "../../../lib/prisma";
+
+const handler = nc<NextApiRequest, NextApiResponse>();
+
+handler.put(async (req, res) => {
+  // WE WILL SEND ENTIRE CART TO THE BACKEND
+
+  const cart = req.body as Record<
+    string,
+    {
+      productId: string;
+      amount: number;
+      countInStock: number;
+      price: number;
+    }
+  >;
+
+  if (!cart) {
+    return res.status(400).send("body is no good");
+  }
+  if (!Object.keys(cart)) {
+    return res.status(400).send("empty cart");
+  }
+
+  for (const key in cart) {
+    const item = cart[key];
+
+    const product = await prismaClient.product.findUnique({
+      where: {
+        productId: item.productId,
+      },
+      select: {
+        countInStock: true,
+      },
+    });
+
+    if (!product) {
+      return res.status(400).send("something wen wrong");
+    }
+
+    await prismaClient.product.update({
+      where: {
+        productId: item.productId,
+      },
+      data: {
+        countInStock: product.countInStock + item.amount,
+      },
+    });
+  }
+
+  return res.status(200).send("cart cleared");
+});
+
+export default handler;
