d9da2acf0cd2696c2ffcee36d70f03581544d6e0
Rade58 <bajic.rade2@gmail.com>
2021-09-17 11:31:47 +0200

    🦉

--- components/7_place_order_page/SummaryList.tsx
+++ components/7_place_order_page/SummaryList.tsx
@@ -196,8 +196,8 @@ const SummaryList: FC = () => {
     if (!total) return;
 
     Cookies.set(TOTAL_PRICE_KEY, total.toFixed(2));
-    Cookies.set(SHIPPING_PRICE_KEY, SHIPPING_PRICE);
-    Cookies.set(TAX_PRICE_KEY, TAX_PRICE);
+    Cookies.set(SHIPPING_PRICE_KEY, SHIPPING_PRICE.toFixed(2));
+    Cookies.set(TAX_PRICE_KEY, TAX_PRICE.toFixed(0));
   }, [total]);
 
   const toto = formatter.format(total);
