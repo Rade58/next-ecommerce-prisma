2dc17f4e574eba7290547469db72e252f77fecbc
Rade58 <bajic.rade2@gmail.com>
2021-09-18 19:02:43 +0200

    🦉

--- pages/api/order/pay/[orderId].ts
+++ pages/api/order/pay/[orderId].ts
@@ -13,13 +13,41 @@ handler.post(async (req, res) => {
   }
 
   try {
-    // LETS GET THE ORDER
-
+    // LETS GET THE ORDER, AND WE WILL GET USERS EMAIL
     const order = await prismaClient.order.findUnique({
       where: {
         id: id as string,
       },
+      include: {
+        buyer: {
+          include: {
+            user: {
+              select: {
+                email: true,
+              },
+            },
+          },
+        },
+      },
     });
+    // IF ORDER DOESN'T EXIST
+
+    if (!order) {
+      return res.status(400).send("order not exist");
+    }
+
+    // NOW WE WOULD TALK TO PAYPAL API
+    // TODO
+
+    // --------------------------------
+
+    // TODO
+    // WHEN WE GET OBJECT FROM PAYPAL
+    // WE CRATE PaymntResult OBJECT
+
+    // TODO
+    // WE THEN UPDATE ORDER BY CONNECTING
+    // PAYMENT RECORD TO THE ORDER RECORD
 
     return res.status(201).send(order);
   } catch (err) {
